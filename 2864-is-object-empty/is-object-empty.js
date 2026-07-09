5f1698a5c1ca338e3f78213ce87187d9fd9306c6
Faizal Sheikh <faizalsheikh399@gmail.com>
2026-07-09 12:50:14 +0530

    Time: 33 ms (95.70%) | Memory: 54.1 MB (45.20%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -2,9 +2,6 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
-var isEmpty = function(obj) {
-   for(let key in obj){
-    return false
-   }
-     return true
+var isEmpty = function (obj) {
+    return Object.keys(obj).length === 0
 };
